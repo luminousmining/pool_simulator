eaec26c51ec0d444140c8420126ec6a5c530f380
luminousminer <150286277+luminousmining@users.noreply.github.com>
2024-12-24 09:59:27 +0100

    simulate autolykos_v2

--- stratums/stratum_autolykos_v2.py
+++ stratums/stratum_autolykos_v2.py
@@ -30,45 +30,63 @@ class StratumAutolykosv2(Stratum):
         else:
             logging.error(f'Unknow method [{method}][{params}]')
 
-    def __on_mining_subscribe(self, sock, request_id):
-        extra_nonce = '0197'
-        body = '{'\
-                 f'"id":{request_id}, ' \
-                 f'"result":[["mining.notify","1405ba371b0d0e4526961935bd5dbeee","EthereumStratum/1.0.0"],' \
-                 f'"{extra_nonce}"],'\
-                 f'"error":null' \
-                 '}'
+    def __on_mining_subscribe(self, sock,  request_id: int or str):
+        extra_nonce = '996e'
+        size = 6
+
+        key_id = f'"id":{request_id}'
+        if type(request_id) is str:
+            key_id = f'"id":"{request_id}"'
+
+        body = '{' \
+               f'{key_id},' \
+               f'"result":[null,\"{extra_nonce}\",{size}],' \
+               f'"error":null' \
+               '}'
         self.send(sock, body)
 
-    def __on_mining_authorize(self, sock, request_id):
+    def __on_mining_authorize(self, sock, request_id: int or str):
+        key_id = f'"id":{request_id}'
+        if type(request_id) is str:
+            key_id = f'"id":"{request_id}"'
+
         body = '{' \
-               f'"id":{request_id},' \
+               f'{key_id},' \
                f'"result":true,' \
                f'"error":null' \
                '}'
         self.send(sock, body)
 
-        difficulty = 0.46565418188038166
+        difficulty = 1
         body = '{' \
-                 '"id":null,' \
-                 '"method":"mining.set_difficulty",' \
-                 f'"params":[{difficulty}]' \
-                 '}'
+               '"id":null,' \
+               '"method":"mining.set_difficulty",' \
+               f'"params":[{difficulty}]' \
+               '}'
         self.send(sock, body)
 
-        params = '["7a2f8335",'\
-                 '"02deafa02fa49495962fa2d5a2cdaa6226b83e98931eaea31ec53e49fe29a14d",' \
-                 '"585f6925be23c7348c95af0f4b7edd4292c075a70ae0e99e3f349356ab3ad090",' \
+        params = '["3",' \
+                 '1415098,' \
+                 '"6f109ba5226d1e0814cdeec79f1231d1d48196b5979a6d816e3621a1ef47ad80",' \
+                 '"",' \
+                 '"",' \
+                 '2,' \
+                 '"28948022309329048855892746252171976963209391069768726095651290785380",' \
+                 '"",' \
                  'true]'
-        body = '{'\
-                '"id":null,'\
-                '"method":"mining.notify",'\
-                f'"params":{params}'\
-                '}'
+        body = '{' \
+               '"id":null,' \
+               '"method":"mining.notify",' \
+               f'"params":{params}' \
+               '}'
         self.send(sock, body)
 
-    def __on_mining_submit(self, sock, request_id: int, params: list):
+    def __on_mining_submit(self, sock, request_id: int or str, params: list):
+        key_id = request_id
+        if type(request_id) is str:
+            key_id = f'"{request_id}"'
+
         logging.info(f'Nonce: {params}')
         body = '{"id":-1,"result":true,"error":null}'
-        body = body.replace('-1', str(request_id))
+        body = body.replace('-1', str(key_id))
         self.send(sock, body)
